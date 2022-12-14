48406d329334dbef39eed5bf7fea45ce5bb7887c
malwaru <malikawarunamal@gmail.com>
2022-12-14 18:04:16 +0100

    Making it ros compatible

--- tools/ros_track.py
+++ tools/ros_track.py
@@ -4,27 +4,33 @@ import os.path as osp
 import time
 import cv2
 import torch
-
 from loguru import logger
-
 from yolox.data.data_augment import preproc
 from yolox.exp import get_exp
 from yolox.utils import fuse_model, get_model_info, postprocess
 from yolox.utils.visualize import plot_tracking
 from yolox.tracker.byte_tracker import BYTETracker
 from yolox.tracking_utils.timer import Timer
+from enum import Enum
 
 
-IMAGE_EXT = [".jpg", ".jpeg", ".webp", ".bmp", ".png"]
 
+########################################
+########################################
+##            TO DO
+########################################
+##  1. Load the parameters from a YAML
+##  2. 
+########################################
+########################################
 
 def make_parser():
     parser = argparse.ArgumentParser("ByteTrack Demo!")
-    parser.add_argument(
-        "demo", default="image", help="demo type, eg. image, video and webcam"
-    )
-    parser.add_argument("-expn", "--experiment-name", type=str, default=None)
-    parser.add_argument("-n", "--name", type=str, default=None, help="model name")
+    # parser.add_argument(
+    #     "demo", default="image", help="demo type, eg. image, video and webcam"
+    # )
+    # parser.add_argument("-expn", "--experiment-name", type=str, default=None)
+    # parser.add_argument("-n", "--name", type=str, default=None, help="model name")
 
     parser.add_argument(
         #"--path", default="./datasets/mot/train/MOT17-05-FRCNN/img1", help="path to images or video"
@@ -39,13 +45,13 @@ def make_parser():
     )
 
     # exp file
-    parser.add_argument(
-        "-f",
-        "--exp_file",
-        default=None,
-        type=str,
-        help="pls input your expriment description file",
-    )
+    # parser.add_argument(
+    #     "-f",
+    #     "--exp_file",
+    #     default=None,
+    #     type=str,
+    #     help="pls input your expriment description file",
+    # )
     parser.add_argument(
         "-c", "--ckpt", default=None, type=str, help="ckpt for eval")
     parser.add_argument(
@@ -90,31 +96,6 @@ def make_parser():
     parser.add_argument('--min_box_area', type=float, default=10, help='filter out tiny boxes')
     parser.add_argument("--mot20", dest="mot20", default=False, action="store_true", help="test mot20.")
     return parser
-
-
-def get_image_list(path):
-    image_names = []
-    for maindir, subdir, file_name_list in os.walk(path):
-        for filename in file_name_list:
-            apath = osp.join(maindir, filename)
-            ext = osp.splitext(apath)[1]
-            if ext in IMAGE_EXT:
-                image_names.append(apath)
-    return image_names
-
-
-def write_results(filename, results):
-    save_format = '{frame},{id},{x1},{y1},{w},{h},{s},-1,-1,-1\n'
-    with open(filename, 'w') as f:
-        for frame_id, tlwhs, track_ids, scores in results:
-            for tlwh, track_id, score in zip(tlwhs, track_ids, scores):
-                if track_id < 0:
-                    continue
-                x1, y1, w, h = tlwh
-                line = save_format.format(frame=frame_id, id=track_id, x1=round(x1, 1), y1=round(y1, 1), w=round(w, 1), h=round(h, 1), s=round(score, 2))
-                f.write(line)
-    logger.info('save results to {}'.format(filename))
-
 
 class Predictor(object):
     def __init__(
@@ -176,77 +157,16 @@ class Predictor(object):
             #logger.info("Infer time: {:.4f}s".format(time.time() - t0))
         return outputs, img_info
 
-
-def image_demo(predictor, vis_folder, current_time, args):
-    if osp.isdir(args.path):
-        files = get_image_list(args.path)
-    else:
-        files = [args.path]
-    files.sort()
-    tracker = BYTETracker(args, frame_rate=args.fps)
-    timer = Timer()
-    results = []
-
-    for frame_id, img_path in enumerate(files, 1):
-        outputs, img_info = predictor.inference(img_path, timer)
-        if outputs[0] is not None:
-            online_targets = tracker.update(outputs[0], [img_info['height'], img_info['width']], exp.test_size)
-            online_tlwhs = []
-            online_ids = []
-            online_scores = []
-            for t in online_targets:
-                tlwh = t.tlwh
-                tid = t.track_id
-                vertical = tlwh[2] / tlwh[3] > args.aspect_ratio_thresh
-                if tlwh[2] * tlwh[3] > args.min_box_area and not vertical:
-                    online_tlwhs.append(tlwh)
-                    online_ids.append(tid)
-                    online_scores.append(t.score)
-                    # save results
-                    results.append(
-                        f"{frame_id},{tid},{tlwh[0]:.2f},{tlwh[1]:.2f},{tlwh[2]:.2f},{tlwh[3]:.2f},{t.score:.2f},-1,-1,-1\n"
-                    )
-            timer.toc()
-            online_im = plot_tracking(
-                img_info['raw_img'], online_tlwhs, online_ids, frame_id=frame_id, fps=1. / timer.average_time
-            )
-        else:
-            timer.toc()
-            online_im = img_info['raw_img']
-
-        # result_image = predictor.visual(outputs[0], img_info, predictor.confthre)
-        if args.save_result:
-            timestamp = time.strftime("%Y_%m_%d_%H_%M_%S", current_time)
-            save_folder = osp.join(vis_folder, timestamp)
-            os.makedirs(save_folder, exist_ok=True)
-            cv2.imwrite(osp.join(save_folder, osp.basename(img_path)), online_im)
-        cv2.imshow("Trcke",result_image)
-        if frame_id % 20 == 0:
-            logger.info('Processing frame {} ({:.2f} fps)'.format(frame_id, 1. / max(1e-5, timer.average_time)))
-
-        ch = cv2.waitKey(0)
-        if ch == 27 or ch == ord("q") or ch == ord("Q"):
-            break
-
-    if args.save_result:
-        res_file = osp.join(vis_folder, f"{timestamp}.txt")
-        with open(res_file, 'w') as f:
-            f.writelines(results)
-        logger.info(f"save results to {res_file}")
-
-
 def imageflow_demo(predictor, vis_folder, current_time, args):
-    cap = cv2.VideoCapture(args.path if args.demo == "video" else args.camid)
+    cap = cv2.VideoCapture(args.camid)
     width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)  # float
     height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)  # float
     fps = cap.get(cv2.CAP_PROP_FPS)
     timestamp = time.strftime("%Y_%m_%d_%H_%M_%S", current_time)
     save_folder = osp.join(vis_folder, timestamp)
     os.makedirs(save_folder, exist_ok=True)
-    if args.demo == "video":
-        save_path = osp.join(save_folder, args.path.split("/")[-1])
-    else:
-        save_path = osp.join(save_folder, "camera.mp4")
+  
+    save_path = osp.join(save_folder, "camera.mp4")
     logger.info(f"video save_path is {save_path}")
     vid_writer = cv2.VideoWriter(
         save_path, cv2.VideoWriter_fourcc(*"mp4v"), fps, (int(width), int(height))
@@ -362,16 +282,40 @@ def main(exp, args):
 
     predictor = Predictor(model, exp, trt_file, decoder, args.device, args.fp16)
     current_time = time.localtime()
-    if args.demo == "image":
-        image_demo(predictor, vis_folder, current_time, args)
-    elif args.demo == "video" or args.demo == "webcam":
-        imageflow_demo(predictor, vis_folder, current_time, args)
+
+    imageflow_demo(predictor, vis_folder, current_time, args)
+
+
+class ByteArgs(Enum):
+    '''
+    The parametes needed for the ByteTracker and  YoloX detector
+    '''
+
+    ##Detector(YoloX) arguments
+    exp_file="exps/example/mot/yolox_tiny_mix_det.py"
+    ckpt="pretrained/bytetrack_ablation.pth.tar"
+    camid=0
+    save_result=False
+    device="gpu"
+    conf=None #Float test confidence
+    nms=None # Float test nms threshold
+    tsize=None # int test image size
+    fps=30 # int frame rate
+    fp16=False # Bool action="store_true", help="Adopting mix precision evaluating
+    fuse=False # Bool  action="store_true",help="Fuse conv and bn for testing
+        
 
 if __name__ == "__main__":
 
-    args = make_parser().parse_args()
-    exp = get_exp(args.exp_file, args.name) 
-    # torch.cuda.empty_cache()
-    # force_cudnn_initialization()
+    # Arguments being used 
+    #  python3 tools/demo_track.py video -f exps/example/mot/yolox_x_mix_det.py -c pretrained/bytetrack_ablation.pth.tar --fp16 --fuse --device cpu
 
+
+    args = make_parser().parse_args()
+    parameters=None
+
+    ##
+
+    exp_file="exps/example/mot/yolox_tiny_mix_det.py"
+    exp = get_exp(exp_file=exp_file,exp_name=None)  
     main(exp, args)
